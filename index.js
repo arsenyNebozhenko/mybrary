c7e1f90a44813169cab75a83a7f86d9289de7a1f
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2019-12-28 21:19:45 +0300

    initial author routes

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,5 +23,7 @@ db.on('error', err => console.log(err))
 db.once('open', () => console.log('Connected to mongoose...'))
 
 app.use('/', require('./routes/index'))
+
+app.use('/authors', require('./routes/authors'))
 
 app.listen(process.env.PORT || 3000)
